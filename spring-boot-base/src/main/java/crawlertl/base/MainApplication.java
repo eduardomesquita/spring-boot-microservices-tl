86ad3c914c06dd6215990d30537a28194443c8ce
Eduardo Mesquita <eduardomesquita09@gmail.com>
2019-01-09 14:21:30 -0200

    refactoring package name

--- spring-boot-base/src/main/java/crawlertl/base/MainApplication.java
+++ spring-boot-base/src/main/java/crawlertl/base/MainApplication.java
@@ -1,4 +1,4 @@
-package crawlertl;
+package crawlertl.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-base/src/main/java/crawlertl/base/controllers/IndexController.java
+++ spring-boot-base/src/main/java/crawlertl/base/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package crawlertl.controllers;
+package crawlertl.base.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
